7d666ffc0aaddb9700788f1707500519ab7ba1d1
Bamashegiba <bamashegiba@gmail.com>
2023-10-22 17:02:15 +0300

    Закомментил cpp

--- Files.cpp
+++ Files.cpp
@@ -8,6 +8,8 @@ void inputFromFile(vector<double>& vec) {
 	string fileName;
 	cout << "Имя файла дожно быть представлено в формате ***.txt" << endl
 		 << "Данные в файле должны быть представлены в виде, например: '-5 34 7 18' " << endl;
+
+	// Зацикленная попытка получения данных из файла для исключения возможных ошибок
 	while (true) {
 		fileName = OpenFile();
 		//size = CalculateSize(fileName);
@@ -39,29 +41,33 @@ string OpenFile() {
 	fileNameChoice choice;
 	string fileName;
 	fstream fileStream;
+
 	cout << "Укажите имя файла: ";
 	getline(cin >> ws, fileName);
+	// Цикл до ввода подходящего файла
 	do {
 		isExit = false;
 		isEmpty = false;
 		fileName = FileNameCheck(fileName);
 		fileStream.open(fileName);
+		// Проверка на наличие данных в файле
 		if (fileStream.is_open() && fileStream.peek() == EOF) {
 			cout << "Файл пуст! Попробуйте снова." << endl;
 			fileStream.close();
 			isExit = false;
 			isEmpty = true;
 		}
+		//Проверха файла на существование
 		else if (!fileStream.is_open()) {
 			cout << endl << "Файл с таким именем не существует! Измените имя файла:" << endl;	
 			getline(cin >> ws, fileName);
 			fileName = FileNameCheck(fileName);
 		}
+		//Корректное открытие файла
 		else if (fileStream.is_open() && !isEmpty) {
 			cout << "Файл открыт!" << endl;
 			isExit = true;
 		}
-
 	} while (!isExit);
 	fileStream.close();
 	return fileName;
@@ -69,7 +75,9 @@ string OpenFile() {
 
 string FileNameCheck(string inputName) {
 	error_code ec{};
-	bool exitFlag = false;
+	bool exitFlag;
+
+	//Цикл до получения корректного имени файла
 	do {
 		exitFlag = true;
 		if (!ifstream(inputName)) continue;
@@ -79,9 +87,7 @@ string FileNameCheck(string inputName) {
 			exitFlag = false;
 			break;
 		}
-
 	} while (!exitFlag);
-
 	return inputName;
 }
 
@@ -89,10 +95,17 @@ bool GetValueFromFile(vector<double>& vec, string fileName) {
 	double tempDouble;
 	bool isCorrect = true;
 	fstream fileStream;
+
 	fileStream.open(fileName);
+	// Цикл по получению данных из файла и их записи в вектор
 	while (true) {
 		fileStream >> tempDouble;
-		if (fileStream.eof()) break;
+		// Если конец файла
+		if (fileStream.eof()) {
+			vec.push_back(tempDouble);
+			break;
+		};
+		// Если не получлось взять данные
 		if (fileStream.fail())
 		{
 			cout << "Данные записаны некорректно." << endl;
